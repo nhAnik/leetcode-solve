9279bb26c7f15bb4362224d517a9639078d296f2
Nayeem Hasan Anik <nayeemhasan.nh01@gmail.com>
2022-01-15 13:00:23 +0600

    Time: 3 ms (54.62%), Space: 63.9 MB (26.67%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,15 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int len = nums.length, end = -1;
-        if (len == 1)
-            return true;
-        for (int i = 0; i < len - 1; i++) {
-            if (end != -1 && i > end)
-                break;
+        int len = nums.length, end = 0;
+        for (int i = 0; i < len; i++) {
+            if (i > end)
+                return false;
             end = Math.max(end, i + nums[i]);
-            if (end >= len - 1)
-                return true;
         }
-        return false;
+        return true;
     }
 }

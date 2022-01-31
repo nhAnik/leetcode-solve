3dc3b0d491bc108a3ad1583c17cad05d5e69d46c
Nayeem Hasan Anik <nayeemhasan.nh01@gmail.com>
2022-01-31 09:09:58 +0600

    Time: 6 ms (43.45%), Space: 43.6 MB (5.42%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -1,9 +1,8 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        int ans = 0;
-        for (int[] account: accounts) {
-            ans = Integer.max(ans, Arrays.stream(account).sum());
-        }
-        return ans;
+        return Arrays.stream(accounts)
+                .mapToInt(account -> Arrays.stream(account).sum())
+                .max()
+                .getAsInt();
     }
 }

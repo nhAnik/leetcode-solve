49fc14f8016f2bf795d5780ff115fc2333d54b6c
Nayeem Hasan Anik <nayeemhasan.nh01@gmail.com>
2022-02-05 18:44:27 +0600

    Time: 196 ms (12.89%), Space: 46.9 MB (11.58%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -10,18 +10,23 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        int len = lists.length;
-        PriorityQueue<ListNode> pq = new PriorityQueue<>(Comparator.comparingInt(l -> l.val));
-        for (int i = 0; i < len; i++)
-            if (lists[i] != null) pq.add(lists[i]);
-        ListNode head, cur;
-        head = cur = new ListNode(-1, null);
-        while (!pq.isEmpty()) {
-            ListNode tmp = pq.poll();
-            cur.next = tmp;
-            cur = cur.next;
-            if (tmp.next != null) pq.add(tmp.next);
+        if (lists.length == 0) return null;
+        for (int i = 1; i < lists.length; i++) {
+            lists[0] = mergeTwo(lists[0], lists[i]);
         }
-        return head.next;
+        return lists[0];
     }
+
+    private ListNode mergeTwo(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwo(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwo(l1, l2.next);
+            return l2;
+        }
+    }
+    
 }

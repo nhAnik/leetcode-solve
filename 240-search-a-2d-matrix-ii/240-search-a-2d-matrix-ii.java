77dae0e822369f2305f5c5693c7da14aee2aef8f
Nayeem Hasan Anik <nayeemhasan.nh01@gmail.com>
2022-07-26 20:39:29 +0600

    Time: 15 ms (21.26%), Space: 57.8 MB (45.10%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,27 +1,32 @@
 class Solution {
+    private int[][] matrix;
+    private int rows;
+    private int cols;
+
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length, n = matrix[0].length;
-        return searchMatrixUtil(matrix, target, 0, 0, m - 1, n - 1);
+        this.matrix = matrix;
+        this.rows = matrix.length;
+        this.cols = matrix[0].length;
+        return searchMatrixUtil(target, 0, 0, rows - 1, cols - 1);
     }
 
-    private boolean searchMatrixUtil(int[][] matrix, int target, int r1, int c1, int r2, int c2) {
-        if (!isValidRow(r1, matrix.length) || !isValidRow(r2, matrix.length)
-                || !isValidCol(c1, matrix[0].length) || !isValidCol(c2, matrix[0].length))
+    private boolean searchMatrixUtil(int target, int r1, int c1, int r2, int c2) {
+        if (!isValidRow(r1) || !isValidRow(r2) || !isValidCol(c1) || !isValidCol(c2))
             return false;
         if (r1 == r2 && c1 == c2) return matrix[r1][c1] == target;
         if (target < matrix[r1][c1] || target > matrix[r2][c2]) return false;
         int rm = (r1 + r2) / 2, cm = (c1 + c2) / 2;
-        return searchMatrixUtil(matrix, target, r1, c1, rm, cm) ||
-                searchMatrixUtil(matrix, target, r1, cm + 1, rm, c2) ||
-                searchMatrixUtil(matrix, target, rm + 1, c1, r2, cm) ||
-                searchMatrixUtil(matrix, target, rm + 1, cm + 1, r2, c2);
+        return searchMatrixUtil(target, r1, c1, rm, cm) ||
+                searchMatrixUtil(target, r1, cm + 1, rm, c2) ||
+                searchMatrixUtil(target, rm + 1, c1, r2, cm) ||
+                searchMatrixUtil(target, rm + 1, cm + 1, r2, c2);
     }
-    
-    private boolean isValidRow(int r, int rows) {
+
+    private boolean isValidRow(int r) {
         return r >= 0 && r < rows;
     }
 
-    private boolean isValidCol(int c, int cols) {
+    private boolean isValidCol(int c) {
         return c >= 0 && c < cols;
     }
 }

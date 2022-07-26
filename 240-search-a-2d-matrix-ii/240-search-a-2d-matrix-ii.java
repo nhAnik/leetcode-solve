3b0db72bfcc49ed53b084ad6e04cfb841dd8871f
Nayeem Hasan Anik <nayeemhasan.nh01@gmail.com>
2022-07-26 21:05:56 +0600

    Time: 11 ms (50.70%), Space: 58.1 MB (25.76%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length, n = matrix[0].length, i = 0;
+        if (target < matrix[0][0] || target > matrix[m - 1][n - 1]) return false;
         for (int[] row: matrix) {
             if (target >= matrix[i][0] && target <= matrix[i][n - 1] &&
                     Arrays.binarySearch(row, target) >= 0) return true;
